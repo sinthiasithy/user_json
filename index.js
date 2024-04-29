1cf5044174ed8562e95089a4ca0df33d743ae426
sinthyasithy <145050483+sinthyasithy@users.noreply.github.com>
2024-04-29 21:03:56 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+let user = {
+    "username": "sin",
+    "id": 123456,
+    "name": "Sinatra",
+    "email": "sinatra@example.com",
+    "age": 35,
+    "address": {
+      "street": "456 Broadway",
+      "city": "New York",
+      "zipcode": "10003",
+      "country": "USA"
+    },
+    "phone": "+11234567890",
+    "website": "https://sinatra.com",
+    "company": {
+      "name": "Jazz Records",
+      "catchPhrase": "Smooth tunes for your soul",
+      "bs": "Creating timeless music"
+    }
+  }
+  console.log(user);
+console.log(user.username);
+console.log(user.id);
+ 
